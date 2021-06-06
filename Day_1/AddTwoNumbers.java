1c6e41b066df46532c35ef6e433b5d0ff3c31d82
Selvam B <84901905+selvam19@users.noreply.github.com>
2021-06-06 17:15:44 +0530

    Update AddTwoNumbers.java

--- Day_1/AddTwoNumbers.java
+++ Day_1/AddTwoNumbers.java
@@ -1,10 +1,8 @@
 public class AddTwoNumbers {
 
 	public static void main(String[] args) {
-		int num1 = 50, num2 = 60, sum;
-		System.out.print("Adding Two Numbers " + num1 + " + " + num2 + "=");
-		System.out.println(num1 + num2);
-
-	}
+        int num1 = 50, num2 = 60, sum;
+        System.out.println("Adding Two Numbers is " + num1 + " + " + num2 + "="+(num1+num2));
+    }
 
 }
